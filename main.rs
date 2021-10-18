b3fdb36effb5a4a91ddb8fbcae9836a4b22f3f81
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-18 15:12:41 +0530

    Initial commit

---
+++ main.rs
@@ -0,0 +1,8 @@
+// This code is editable, feel free to hack it!
+// This is the main function
+fn main() {
+    // Statements here are executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+}
